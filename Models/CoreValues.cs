79f5af8f949f84089e0c50069ef9f4d5b15b87f8
Bruce Cosgray <bc358116@ohio.edu>
2021-10-21 17:46:09 -0400

    Added models, controllers and views

---
+++ Models/CoreValues.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200_Team6.Models
+{
+    public class CoreValues
+    {
+        public int CoreValueID { get; set; }
+
+        public string CoreValue { get; set; }
+
+        public string CVDesc { get; set; }
+
+        public ICollection<UserData>  userdata { get; set; }
+    }
+}
---
+++ Models/Recognition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200_Team6.Models
+{
+    public class Recognition
+    {
+        public int CoreValueID { get; set; }
+
+        public Guid ID { get; set; }
+
+        public DateTime RecDate { get; set; }
+
+        public int GiveID { get; set; }
+
+        public int RecID { get; set; }
+
+        public virtual UserData userdatas { get; set; }
+
+    }
+}
